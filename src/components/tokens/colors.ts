238513ac46fd1415e64b3694960221a385613585
bdasgupta02 <bdasgupta02@gmail.com>
2022-05-12 21:34:06 +0800

    setting up lato as default font

---
+++ src/components/tokens/colors.ts
@@ -0,0 +1,16 @@
+const Colors = {
+    Primary: '#72B1F1',
+    Secondary: {
+        SuperPink: '#D668A9',
+        SlateBlue: '#7881F9',
+        QueenBlue: '#486C8F',
+    },
+    Neutral: {
+        Background: '#111c26',
+        LightGray: '#D3D3D3',
+        AntiFlashWhite: '#F3F3F3',
+        White: '#FFFFFF',
+    },
+}
+
+export default Colors;
